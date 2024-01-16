ee1cc2b8f696b0fb91b40e3d90f0d34d3d46d29e
MaxiCode <andresholsbach@gmail.com>
2024-01-16 11:29:50 -0300

    LIMPIANDO LA CONSOLA

--- Bitacora/models.py
+++ Bitacora/models.py
@@ -2,13 +2,6 @@ from operator import truediv
 from pyexpat import model
 from statistics import mode
 from django.db import models
-from django.contrib.auth.models import User
-
-"""
-class Avatar(models.Model):
-    user = models.Foreignkey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="avatars", null=True, blank=True)
- """   
 
 class BitacoraModel(models.Model):
     modelo = models.CharField(max_length=100)
@@ -17,8 +10,5 @@ class BitacoraModel(models.Model):
     trabajos = models.TextField()
     tecnico = models.CharField(max_length=100)
     finalizado = models.DateField(auto_now_add=True)
-
-
-    
 
 # Create your models here.
